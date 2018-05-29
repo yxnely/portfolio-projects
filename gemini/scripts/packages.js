f9653cf226bf72148afc5ff3bc463ddc13d1f851
Yanely <yanely_rmz@yahoo.com>
2018-05-28 23:33:21 -0400

    update to look and filtering/unfiltering list

--- gemini/scripts/packages.js
+++ gemini/scripts/packages.js
@@ -1,17 +1,53 @@
 const geminiApp = new Vue({
 	el: '#app',
 	data: {
-        items: []
-,    },
+        packages: [],
+        statuses: []
+    },
     created() {
         fetch('https://yxnely.github.io/portfolio-projects/gemini/data/packages.json', {
             method: 'GET'
         }).then(res => res.json())
         .then(res => {
-            geminiApp.items = res
+            geminiApp.statuses = res.status
         })
         .catch(err => {
             console.log(err)
         })
     },
+    methods: {
+        getAllPackages() {
+            return fetch('https://yxnely.github.io/portfolio-projects/gemini/data/packages.json', {
+                method: 'GET'
+            })
+        },
+        unfilter() {
+            this.getAllPackages()
+            .then(res => res.json())
+            .then(res => {
+                geminiApp.packages = []
+                
+                geminiApp.packages = res.packages
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        },
+        filter(status) {
+            this.getAllPackages()
+            .then(res => res.json())
+            .then(res => {
+                geminiApp.packages = []
+
+                res.packages.forEach(pkg => {
+                    if (pkg.status == status) {
+                        geminiApp.packages.push(pkg);
+                    }
+                })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+    }
 })
