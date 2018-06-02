3a7108509d528b39d5c9022e1bfd443b07ce44c0
Yanely <yanely_rmz@yahoo.com>
2018-06-01 21:24:03 -0400

    updating the change of selection

--- speedy/js/main.js
+++ speedy/js/main.js
@@ -12,13 +12,6 @@ const speedyApp = new Vue({
                 chosen: ''
             },
             {
-                id: 2,
-                method: 'chooseRestaurant',
-                text: 'Restaurant',
-                icon: 'fa fa-building',
-                chosen: ''
-            },
-            {
                 id: 3,
                 method: 'chooseOrder',
                 text: 'Order',
@@ -34,8 +27,8 @@ const speedyApp = new Vue({
             }
         ],
         types: [],
-        currSelection: null,
-        deliveryTypes: [
+        currSelection: '',
+        deliveryOptions: [
             {
                 id: 1,
                 name: 'Restaurant'
@@ -47,6 +40,53 @@ const speedyApp = new Vue({
             {
                 id: 3,
                 name: 'Grocery Store'
+            }
+        ],
+        storeOptions: [
+            {
+                id: 1,
+                name: 'Red Robin',
+                type: 'Restaurant'
+            },
+            {
+                id: 2,
+                name: 'Olive Garden',
+                type: 'Restaurant'
+            },
+            {
+                id: 3,
+                name: 'Red Robin',
+                type: 'Restaurant'
+            },
+            {
+                id: 4,
+                name: 'Walmart',
+                type: 'Grocery Store'
+            },
+            {
+                id: 5,
+                name: 'Winn-Dixie',
+                type: 'Grocery Store'
+            },
+            {
+                id: 6,
+                name: 'Walgreens',
+                type: 'Grocery Store'
+            },
+            {
+                id: 7,
+                name: 'McDonalds',
+                type: 'Fast Food'
+            },
+            {
+                id: 8,
+                name: 'Sonic',
+                type: 'Fast Food'
+            },
+            {
+                id: 9,
+                name: 'Wendys\'s',
+                type: 'Fast Food'
             }
         ]
     },
@@ -68,14 +108,14 @@ const speedyApp = new Vue({
             }
         },
         chooseDeliveryType(methodId) {
-            this.types = this.deliveryTypes
+            this.types = this.deliveryOptions
             this.currSelection = methodId
 
             this.showOptions = true
             this.showOverview = false
         },
-        chooseRestaurant() {
-            console.log('chooseRestaurant')
+        chooseStore() {
+            console.log('chooseStore')
         },
         chooseOrder() {
             console.log('chooseOrder')
@@ -84,9 +124,30 @@ const speedyApp = new Vue({
             console.log('addPayment')
         },
         itemChosen(id) {
-            console.log(currSelection)
-            // const chosenItem = options.map(e => e.id).indexOf(currSelection)
-            // console.log(chosenItem)
+            const chosenItem = this.options.map(e => e.id).indexOf(this.currSelection)
+            const typeSelection = this.types.map(e => e.id).indexOf(id)
+            
+            this.options[chosenItem].chosen = this.types[typeSelection]
+
+            const newOption = {
+                id: 2,
+                method: 'chooseStore',
+                text: this.types[typeSelection].name,
+                type: this.types[typeSelection].name,
+                icon: 'fa fa-building',
+                chosen: ''
+            }
+
+            /* 
+                Insert to the original options
+                to appear on the main order screen
+            */
+           this.options.length > 3 ? this.options.splice(1, 1, newOption) : this.options.splice(1, 0, newOption)
+            
+
+            // Return to original options screen
+            this.showOptions = false
+            this.showOverview = true
         }
     }
 })
